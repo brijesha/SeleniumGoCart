0f3d00698ff546f9d50f660ffbad6480ac79c049
Brijesha Mistry <brijeshaa@gmail.com>
2021-10-13 11:37:54 -0700

    Organised pageObjects page

--- src/test/java/pageObjects/HomePageObjects.java
+++ src/test/java/pageObjects/HomePageObjects.java
@@ -1,15 +1,10 @@
 package pageObjects;
 
-import static org.testng.Assert.fail;
-
-import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class HomePageObjects {
-	// String[] productArry = { "Brocolli", "Potato", "Mango" };
+
 	WebDriver driver;
 
 	By cartImg = By.cssSelector("img[alt='Cart']");
@@ -22,35 +17,19 @@ public class HomePageObjects {
 	}
 
 	public void cartImgClick() {
-		getElem(cartImg, "Cart").click();
+		driver.findElement(cartImg).click();
 	}
 
 	public String getEmptyCartMsg() {
-		return getElem(emptyCartMsg, "Cart message").getText();
+		return driver.findElement(emptyCartMsg).getText();
 	}
 
 	public String getItemsText() {
-		return getElem(items, "Items").getText();
+		return driver.findElement(items).getText();
 	}
 
 	public String getPriceText() {
-		return getElem(price, "Price").getText();
+		return driver.findElement(price).getText();
 	}
 
-	public WebElement getElem(By locator, String elemName) {
-		List<WebElement> elems = driver.findElements(locator);
-		if (elems.isEmpty()) {
-			fail(elemName + " element not found");
-		}
-		return elems.get(0);
-	}
-	/*
-	 * // Check element value is zero public void validateElemIntZero(String
-	 * elemName) { WebElement elem = getElem(items, elemName); String elemText =
-	 * elem.getText(); int elemInt = 0; try { elemInt = Integer.parseInt(elemText);
-	 * } catch (NumberFormatException e) { fail(elemName +
-	 * " value is not an integer"); } assertEquals(elemInt, 0, elemName +
-	 * " value is not zero"); }
-	 * 
-	 */
 }
