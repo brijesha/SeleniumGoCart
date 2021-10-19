281e576988f6158a3269c72b67dea510db1bd4db
Brijesha Mistry <brijeshaa@gmail.com>
2021-10-19 10:36:12 -0700

    make utility file

---
+++ src/test/java/util/Utility.java
@@ -0,0 +1,17 @@
+package util;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.fail;
+
+public class Utility {
+	
+	public static void checkStrEqualsInt(String str, int expectedValue, String elemName) {
+		int strInt = 0;
+		try {
+			strInt = Integer.parseInt(str);
+		} catch (NumberFormatException e) {
+			fail(elemName + " is not an integer");
+		}
+		assertEquals(strInt, expectedValue, elemName + " not equals to " + expectedValue);
+	}
+}
